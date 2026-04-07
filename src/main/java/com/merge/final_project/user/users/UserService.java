09195ad6a4658154e190c45aef49d13bfa09ac9e
lcw <dlcodnjs667@gmail.com>
2026-04-07 21:47:09 +0900

    chore:[이채원] 임시저장

--- src/main/java/com/merge/final_project/user/users/UserService.java
+++ src/main/java/com/merge/final_project/user/users/UserService.java
@@ -4,6 +4,20 @@ import com.merge.final_project.user.signUp.dto.UserSignUpRequestDTO;
 import com.merge.final_project.user.users.dto.login.UserLoginRequestDTO;
 
 public interface UserService {
+    //1. 로그인 용도
     public String login(UserLoginRequestDTO dto);
+
+    //2.로그인 -부가기능 1: 아이디 찾기 (핸드폰 번호+ 이름)
+
+    //3.로그인- 부가기능 2: 비밀번호 재설정 ( 로그인전에 비밀번호를 잃어버렸을 경우에 )
+
+    //4.마이페이지 -부가기능 : 비밀번호 변경 (마이페이지에서 변경하는 경우)
+
+    //4. 마이페이지 -1. 개인정보 불러오기 :
+    //5. 마이페이지 -1. 계정별 기부 결과 불러오기:
+    //6. 기부내역 -1. 기부 증서 조회하기
+    //7. 마이페이지 -2. 찜 등록
+    //8. 마이페이지 -3. 찜 해제
+    //9. 마이페이지 4- 찜 조회
 
 }
