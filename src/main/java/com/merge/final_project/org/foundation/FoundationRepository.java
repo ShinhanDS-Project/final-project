b46347cc534e147a67f3ccacaa8631c82bbe353e
lcw <dlcodnjs667@gmail.com>
2026-04-08 11:03:17 +0900

    fix:[이채원] 패키지 구조 수정

--- src/main/java/com/merge/final_project/org/foundation/FoundationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.merge.final_project.org.foundation;
-
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FoundationRepository extends JpaRepository<Foundation, Long> {
-}
