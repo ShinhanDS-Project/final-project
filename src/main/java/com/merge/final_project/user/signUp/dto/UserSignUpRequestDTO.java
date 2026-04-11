5165a4b6e42de17001379da22969cca4c6c4c2d8
Chaewon.Lee <dlcodnjs667@gmail.com>
2026-04-12 05:00:29 +0900

    fixeature:[이채원] 비밀번호 정책 생성

--- src/main/java/com/merge/final_project/user/signUp/dto/UserSignUpRequestDTO.java
+++ src/main/java/com/merge/final_project/user/signUp/dto/UserSignUpRequestDTO.java
@@ -1,10 +1,8 @@
 package com.merge.final_project.user.signUp.dto;
 
+import com.merge.final_project.global.exceptions.NoRepeatedDigits;
 import com.merge.final_project.user.users.LoginType;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -15,20 +13,28 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class UserSignUpRequestDTO {
 
-
+   @NotBlank(message = "이메일은 필수입니다.")
    @Email(message = "이메일 형식이 올바르지 않습니다.")
    private String email;
-
+   @NotBlank(message = "비밀번호는 필수입니다.")
+   @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야 해.")
+   @Pattern(
+           regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).*$",
+           message = "비밀번호는 영문, 숫자, 특수문자를 각각 1개 이상 포함해야 해."
+   )
+   @NoRepeatedDigits(message = "같은 숫자를 3번 이상 연속으로 사용할 수 없어.")
    private String password;
 
-
+   @NotBlank(message = "이름은 필수입니다.")
    private String name;
 
+   @NotBlank(message = "휴대폰 번호는 필수입니다.")
    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 형식과 맞지 않습니다. xxx-xxxx-xxxx 또는 xx-xxx-xxxx 형태여야 합니다.")
    private String phone;
 
    @NotNull(message = "생년월일은 필수입니다.")
    private LocalDate birth;
+
 
    private LoginType loginType;
 
