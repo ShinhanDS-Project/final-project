daaecd62008353871578aa4a3e27ae392bfbab51
Gabee-ni <been.dev,kim@gmail.com>
2026-04-13 16:09:20 +0900

    fix: 에러코ㅡ드 중복 해결

--- src/main/java/com/merge/final_project/global/exceptions/ErrorCode.java
+++ src/main/java/com/merge/final_project/global/exceptions/ErrorCode.java
@@ -5,9 +5,6 @@ import org.springframework.http.HttpStatus;
 
 @Getter
 public enum ErrorCode {
-    // 사용자
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_001", "존재하지 않는 사용자입니다."),
-
     AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, "ADMIN_001","아이디 또는 비밀번호가 올바르지 않습니다."),
     // 보안을 위해 외부로는 안 남기지만 내부 로깅 용으로 남길 코드들.
     ADMIN_NOT_FOUND(HttpStatus.NOT_FOUND, "ADMIN_002", "존재하지 않는 관리자입니다."),
