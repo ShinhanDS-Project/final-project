d9a418824367b26c4203a61093a4a22741954e60
euiseon34 <euiseon5895@gmail.com>
2026-04-13 09:47:32 +0900

    [임시 저장]

--- src/main/java/com/merge/final_project/admin/auth/AdminAuthService.java
+++ src/main/java/com/merge/final_project/admin/auth/AdminAuthService.java
@@ -2,7 +2,9 @@ package com.merge.final_project.admin.auth;
 
 import com.merge.final_project.admin.auth.dto.AdminSigninRequestDTO;
 import com.merge.final_project.admin.auth.dto.AdminSigninResponseDTO;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface AdminAuthService {
     AdminSigninResponseDTO login(AdminSigninRequestDTO requestDTO);
 
